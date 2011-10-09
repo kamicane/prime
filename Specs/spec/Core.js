74bc5ab5f1db3c10f5cfeb82c5774a21ceeb4ac3
Arian <arian@aryweb.nl>
2011-10-09 13:47:26 +0200

    100% Specs for Core.js

--- Specs/spec/Core.js
+++ Specs/spec/Core.js
@@ -1,4 +1,22 @@
 
 define(['Base/Core'], function(Core){
 
+	describe('Core', function(){
+
+		var toString = Object.prototype.toString;
+		beforeEach(function(){
+			this.addMatchers({
+				toBeString: function() { return toString.call(this.actual) == '[object String]'; }
+			});
+		});
+
+		it('should have a version', function(){
+			expect(Core.version).toBeString();
+		});
+
+		it('should have a build hash', function(){
+			expect(Core.build).toBeString();
+		});
+
+	});
 });
