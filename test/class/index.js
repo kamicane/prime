8729d0c19b9bfd390208e823d147c9e4da946ccf
Valerio Proietti <kamicane@gmail.com>
2012-03-16 17:38:31 +0100

    - updated prime specs

--- test/class/index.js
+++ test/class/index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 var expect = require('expect.js')
-var Class = require('../../class'),
+var Class = require('../../prime'),
 	create = require("../../util/create");
 
 var Animal = Class({
