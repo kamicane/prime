5b255deedebb5228650f930ab6f6807238edaead
Valerio Proietti <kamicane@gmail.com>
2012-03-19 16:50:10 +0100

    updated main

--- main.js
+++ main.js
@@ -1,74 +1,49 @@
-// require pretty much everything that isnt internal
-"use strict"
+/*
+main
+- rquire everything, attach to prime
+*/"use strict"
 
 var prime = require("./prime")
-
 var type = require("./util/type")
 
-var array = require("./types/array")
-var object = require("./types/object")
+var map = require("./collection/map")
+var list = require("./collection/list")
+var hash = require("./collection/hash")
+
 var string = require("./types/string")
-var method = require("./types/method")
 var number = require("./types/number")
-var regexp = require("./types/regexp")
-
-var Map = require("./util/map")
-var Emitter = require("./util/emitter")
-
-// instantiate a new ghost
 
 var ghost = require("./util/ghost")()
 
-// and register ghosts
+prime.type = type
+
+prime.map = map
+prime.list = list
+prime.hash = hash
+
+prime.string = string
+prime.number = number
+
+prime.ghost = ghost
 
 ghost.register(function(self){
-	return type(self) === "array"
-}, array)
+	return self && (type(self) === "array" || type(self.length) === "number")
+}, list)
 
 ghost.register(function(self){
-	return type(self) === "list"
-}, array)
-
-ghost.register(function(self){
-	return type(self) === "string"
-}, string)
-
-ghost.register(function(self){
-	return type(self) === "regexp"
-}, regexp)
-
-ghost.register(function(self){
-	return type(self) === "method"
-}, method)
+	return type(self) === "object"
+}, hash)
 
 ghost.register(function(self){
 	return type(self) === "number"
 }, number)
 
 ghost.register(function(self){
-	return type(self) === "object"
-}, object)
+	return type(self) === "string"
+}, string)
 
 ghost.register(function(self){
-	return type(self) === "list" && type(self._values) === "array" && type(self._keys) === "array"
-}, Map)
-
-// attach stuff to prime
-
-prime.array = array
-prime.number = number
-prime.string = string
-prime.object = object
-prime.regexp = regexp
-prime.method = method
-
-prime.type = type
-
-prime.Map = Map
-prime.Emitter = Emitter
-
-prime.ghost = ghost
-
-// export prime
+	return self && self.toString() == "[object Map]"
+}, map)
 
 module.exports = prime
