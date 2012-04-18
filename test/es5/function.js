c62278e0ad36fa567e519a6e666c6e2748e0f319
Arian <arian@aryweb.nl>
2012-04-18 22:05:28 +0200

    Add ES5 tests for function, number, regexp and string

---
+++ test/es5/function.js
@@ -0,0 +1,40 @@
+"use strict"
+
+var expect = require('expect.js')
+var fn = require('../../es5/function')
+
+describe('function', function(){
+
+	it('should implement .apply', function(){
+		var context = {}
+		fn.apply(function(a, b){
+			expect(this).to.be(context)
+			expect(a).to.be(1)
+			expect(b).to.be(2)
+		}, context, [1, 2])
+	})
+
+	it('should implement .call', function(){
+		var context = {}
+		fn.call(function(a, b){
+			expect(this).to.be(context)
+			expect(a).to.be(1)
+			expect(b).to.be(2)
+		}, context, 1, 2)
+	})
+
+	it('should implement .bind', function(){
+		var context = {}, calls = 0
+		var fun = function(){
+			expect(this).to.be(context)
+			calls++
+		}
+		var bound = fn.bind(fun, context)
+		bound()
+		expect(calls).to.be(1)
+	})
+
+	// TODO implement all other .bind tests
+
+})
+
---
+++ test/es5/regexp.js
@@ -0,0 +1,13 @@
+"use strict"
+
+var expect = require('expect.js')
+var regexp = require('../../es5/regexp')
+
+describe('regexp', function(){
+
+	it('should implement native regexp methods', function(){
+		expect(regexp.test(/a/, 'a')).to.be.ok()
+		expect(regexp.exec(/a/, 'a').join()).to.eql('a')
+	})
+
+})
