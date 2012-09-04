b2b2a76905ca3395c3055876e6512ab714e5efb5
Valerio Proietti <kamicane@gmail.com>
2012-09-04 15:28:30 +0200

    - emitter uses an object  - added cleanup for empty _listeners

--- util/emitter.js
+++ util/emitter.js
@@ -5,33 +5,57 @@ Emitter
 var prime = require("../prime"),
     array = require("../es5/array")
 
+var EID = 0
+
 module.exports = prime({
 
     on: function(event, fn){
         var listeners = this._listeners || (this._listeners = {}),
-            events = listeners[event] || (listeners[event] = [])
-        if (!events.length || array.indexOf(events, fn) === -1) events.push(fn)
+            events = listeners[event] || (listeners[event] = {})
+
+
+        var exists = false
+        for (var k in events) if (events[k] === fn){
+            exists = true
+            break
+        }
+
+        if (!exists) events[(EID++).toString(36)] = fn
         return this
     },
 
     off: function(event, fn){
-        var listeners = this._listeners, events
-        if (listeners && (events = listeners[event]) && events.length){
-            var index = array.indexOf(events, fn)
-            if (index > -1) delete events[index]
+        var listeners = this._listeners, events, key, k, l, empty, length = 0
+        if (listeners && (events = listeners[event])){
+
+            for (k in events){
+                length++
+                if (key == null && events[k] === fn) key = k
+                if (key && length > 1) break
+            }
+
+            if (key){
+                delete events[key]
+                if (length === 1){
+                    delete listeners[event]
+
+                    empty = true
+                    for (l in listeners){
+                        empty = false
+                        break
+                    }
+                    if (empty) delete this._listeners
+                }
+            }
         }
         return this
     },
 
     emit: function(event){
         var listeners = this._listeners, events
-        if (listeners && (events = listeners[event]) && events.length){
+        if (listeners && (events = listeners[event])){
             var args = (arguments.length > 1) ? array.slice(arguments, 1) : []
-            for (var i = 0; i < events.length; i++){
-                var evt = events[i]
-                if (evt) evt.apply(this, args)
-                else events.splice(i--, 1)
-            }
+            for (var k in events) events[k].apply(this, args)
         }
         return this
     }
