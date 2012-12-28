3f9daa62fff26592693e460a3fad68df3d391355
Valerio Proietti <kamicane@gmail.com>
2012-12-28 23:03:25 +0100

    better naming for deferred/promise.
    fulfill is now accept, which is an antonym of reject.
    and you know how much I love antonyms.
    the promise is now named.

--- test/promise.js
+++ test/promise.js
@@ -4,14 +4,22 @@ var promise = require("../util/promise.js")
 
 exports.fulfilled = function(value){
     var deferred = promise()
-    deferred.fulfill(value)
+    deferred.accept(value)
     return deferred.promise
-};
+}
 
 exports.rejected = function(reason){
     var deferred = promise()
     deferred.reject(reason)
     return deferred.promise
-};
+}
 
-exports.pending = promise
+exports.pending = function(){
+    var deferred = promise()
+
+    return {
+        promise: deferred.promise,
+        fulfill: function(value){ return deferred.accept(value) },
+        reject: function(reason){ deferred.reject(reason) }
+    }
+}
--- util/promise.js
+++ util/promise.js
@@ -3,20 +3,34 @@ promise
  - code based on https://github.com/ForbesLindesay/promises-a MIT Licensed
 */"use strict"
 
+var prime = require("../prime/")
+
 var defer = (global.process && process.nextTick) || function(callback){
     setTimeout(callback, 0)
 }
 
-var states = {PENDING: 0, FULFILLED: 1, REJECTED: 2}
+// states of the promise
 
-var promise = function(){
+var states = {PENDING: 0, FULFILLED: 1, FAILED: 2}
 
-    var self    = {},
-        waiting = [],
-        state   = states.PENDING,
+// promise() returns a new deferred
+// the deferred object can be either rejected or accepted
+// if the deferred is rejected, the promise is failed
+// if the deferred is accepted, the promise is fulfilled
+// the promise is accessed with the .promise property of the deferred object
+// the promise is an object with a method called .then
+
+var deferred = function deferred(){
+
+    if (!(this instanceof deferred)) return new deferred()
+
+    var state = states.PENDING,
+        stack = [],
         value
 
-    // var pub = {then: then}
+    var next = function(delayed){
+        if (stack.length) stack.shift()(delayed)
+    }
 
     var resolve = function(st, val){
         state = st
@@ -24,32 +38,27 @@ var promise = function(){
         next()
     }
 
-    var next = function(deferred){
-        if (waiting.length) waiting.shift()(deferred)
-    }
-
-    var fulfill = self.fulfill = function(val){
+    var accept = this.accept = function(val){
         if (!state){
-
             // if the promise gets fulfilled with another promise
-            // p1.fulfill(promise) === promise.then(p1.fulfill, p1.reject)
-
-            if (val && typeof val.then === 'function') val.then(fulfill, reject)
+            // p1.accept(promise) === promise.then(p1.accept, p1.reject)
+            if (val && typeof val.then === "function") val.then(accept, reject)
             else resolve(states.FULFILLED, val)
         }
-
         return this
     }
 
-    var reject = self.reject = function(reason){
-        if (!state) resolve(states.REJECTED, reason)
+    var reject = this.reject = function(reason){
+        if (!state) resolve(states.FAILED, reason)
         return this
     }
 
-    var then = self.then = function(success, err){ // then always returns a new promise
-        var p = promise()
+    var promise = function promise(){}
 
-        var done = function(deferred){ // this function gets pushed in the waiting stack
+    var then = this.then = promise.prototype.then = function(success, err){ // then always returns a new promise
+        var deferred = new deferred()
+
+        var done = function(delayed){ // this function gets pushed in the waiting stack
 
             var callback = (state === states.FULFILLED) ? success : err
 
@@ -57,42 +66,38 @@ var promise = function(){
 
                 var exec = function(){
                     try { // fulfill the new promise with the result of the success callback
-                        p.fulfill(callback(value))
+                        deferred.accept(callback(value))
                     } catch (err){ // unless the success callback throws an error, in which case reject it with the error
-                        p.reject(err)
+                        deferred.reject(err)
                     }
-                    // next then. we are certain that we are in a deferred state here, so force it to not defer again
+                    // next then. we are certain that we are in a delayed state here, so force it to not defer again
                     next(true)
                 }
 
-                deferred ? exec() : defer(exec) // if we're not already in a deferred state, defer the fulfillment / rejection
+                delayed ? exec() : defer(exec) // if we're not already in a delayed state, defer the fulfillment / rejection
 
             } else if (state === states.FULFILLED){
                 // if no callback is found and the promise is fulfilled, fulfill the new promise with the current promise's value
-                p.fulfill(value)
-
-                // next then, defer only when not in a deferred state
-                next(deferred)
+                deferred.accept(value)
+                // next then, defer only when not in a delayed state
+                next(delayed)
             } else {
-                // if no callback is found and the promise is either rejected or pending, reject the new promise with the current promise's value
-                p.reject(value)
-
-                // next then, defer only when not in a deferred state
-                next(deferred)
+                // if no callback is found and the promise is either failed or pending, reject the new promise with the current promise's value
+                deferred.reject(value)
+                // next then, defer only when not in a delayed state
+                next(delayed)
             }
         }
 
-        waiting.push(done)
-
+        stack.push(done)
         // if the promise has a state, proceed to the next then
         if (state) next()
 
-        return p.promise
+        return deferred.promise
     }
 
-    self.promise = {then: then}
+    this.promise = new promise
 
-    return self
 }
 
-module.exports = promise
+module.exports = prime({constructor: deferred})
