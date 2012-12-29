47d524c765ce1603d95e0820a570846ebaaa1a17
Arian Stolwijk <stolwijk.arian@gmail.com>
2012-12-29 13:57:39 +0100

    Add tests for es5/object

---
+++ test/es5/object.js
@@ -0,0 +1,18 @@
+"use strict"
+
+var expect = require('expect.js')
+var object = require('../../es5/object')
+
+describe('es5/object', function(){
+
+    it('should have exported a value', function(){
+        expect(object != null).to.be.ok()
+    })
+
+    it('should implement existing methods in the shell', function(){
+        expect(object.hasOwnProperty).to.be.ok()
+        expect(object.hasOwnProperty != Object.prototype.hasOwnProperty).to.be.ok()
+        expect(object.toString([])).to.be('[object Array]')
+    })
+
+})
