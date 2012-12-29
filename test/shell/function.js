76964484a28755bb7285f96adcb164c39952515c
Arian Stolwijk <stolwijk.arian@gmail.com>
2012-12-29 14:03:54 +0100

    Add shell/function test

---
+++ test/shell/function.js
@@ -0,0 +1,15 @@
+"use strict"
+
+var expect = require('expect.js')
+var fn = require('../../shell/function')
+
+describe('shell/function', function(){
+
+    it('should export the function shell', function(){
+        expect(fn != null).to.be.ok()
+        expect(fn.apply(function(){
+            return 1
+        })).to.be.a('number')
+    })
+
+})
