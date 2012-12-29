75bd3062b6e3b7d028dede5fb223a822d9511faf
Arian Stolwijk <stolwijk.arian@gmail.com>
2012-12-29 14:01:29 +0100

    Add shell/date test

---
+++ test/shell/date.js
@@ -0,0 +1,13 @@
+"use strict"
+
+var expect = require('expect.js')
+var date = require('../../shell/date')
+
+describe('shell/date', function(){
+
+    it('should export the date shell', function(){
+        expect(date != null).to.be.ok()
+        expect(date.now()).to.be.a('number')
+    })
+
+})
