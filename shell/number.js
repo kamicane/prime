f7567fa10390dd13e72ac1a36d3380dff547723d
Arian Stolwijk <stolwijk.arian@gmail.com>
2013-03-02 12:06:06 +0100

    Shells now use the implementGenerics method
    This optimizes for the common generic case. No method wrapping as generics

--- shell/number.js
+++ shell/number.js
@@ -5,25 +5,24 @@ number
 
 var number = require("../es5/number")
 
-module.exports = number.implement({
+module.exports = number.implementGenerics({
 
-    limit: function(min, max){
-        return Math.min(max, Math.max(min, this));
+    limit: function(self, min, max){
+        return Math.min(max, Math.max(min, self));
     },
 
-    round: function(precision){
+    round: function(self, precision){
         precision = Math.pow(10, precision || 0).toFixed(precision < 0 ? -precision : 0)
-        return Math.round(this * precision) / precision
+        return Math.round(self * precision) / precision
     },
 
-    times: function(fn, context){
-        for (var i = 0; i < this; i++) fn.call(context, i, null, this)
-        return this
+    times: function(self, fn, context){
+        for (var i = 0; i < self; i++) fn.call(context, i, null, self)
+        return self
     },
 
-    random: function(max){
-        return Math.floor(Math.random() * (max - this + 1) + this)
+    random: function(self, max){
+        return Math.floor(Math.random() * (max - self + 1) + self)
     }
-
 
 })
