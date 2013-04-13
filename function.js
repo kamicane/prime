8b0626d9ea828e6ff8f5041a8c45a5f02b5495fc
Valerio Proietti <kamicane@gmail.com>
2013-04-13 12:11:00 +0200

    function shell

---
+++ function.js
@@ -0,0 +1,20 @@
+/*
+function
+*/"use strict"
+
+var function_ = require("../shell")["function"]
+var proto = Function.prototype
+
+function_.implement({
+    apply:    proto.apply
+    call:     proto.call
+    toString: proto.toString
+})
+
+function_.extend({
+    bind:     require("./function/bind"),
+    throttle: require("./function/throttle"),
+    debounce: require("./function/debounce")
+})
+
+module.exports = function_
