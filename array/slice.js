0a7a33055e78bf75885c172a5f73751297b1fd27
Valerio Proietti <kamicane@spotify.com>
2013-04-25 14:11:38 +0200

    better slice

--- array/slice.js
+++ array/slice.js
@@ -2,10 +2,13 @@
 array:slice
 */"use strict"
 
-var proto = Array.prototype.slice
-
 var slice = function(self, begin, end){
-    return proto.call(self, begin, end)
+    var length = self.length
+    if (begin == null) begin = 0
+    end = (end == null) ? length : ((end %= length) < 0) ? end + length : end;
+    var slit = []
+    for (var i = begin; i < end; i++) slit.push(self[i])
+    return slit
 }
 
 module.exports = slice
