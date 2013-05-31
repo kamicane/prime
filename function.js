18f1061d76793c57fc2501e2fc131ba873f31623
Valerio Proietti <kamicane@spotify.com>
2013-05-31 12:17:57 +0200

    removed non-implemented methods.

--- function.js
+++ function.js
@@ -6,15 +6,13 @@ var function_ = require("./_shell")["function"]
 var proto = Function.prototype
 
 function_.implement({
-    apply:    proto.apply
-    call:     proto.call
+    apply:    proto.apply,
+    call:     proto.call,
     toString: proto.toString
 })
 
 function_.extend({
-    bind:     require("./function/bind"),
-    throttle: require("./function/throttle"),
-    debounce: require("./function/debounce")
+    bind:     require("./function/bind")
 })
 
 module.exports = function_
