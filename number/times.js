a4c5d0f9a852b72d7eb064dfc8e1d08489ad51a2
Valerio Proietti <kamicane@spotify.com>
2013-05-31 12:16:32 +0200

    number/times should return when the callback returns false.

--- number/times.js
+++ number/times.js
@@ -3,7 +3,9 @@ number:times
 */"use strict"
 
 var times = function(self, method, context){
-    for (var i = 0; i < this; i++) method.call(context, i, null, self)
+    for (var i = 0; i < self; i++){
+        if (method.call(context, i, null, self) === false) break
+    }
     return self
 }
 
