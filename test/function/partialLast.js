4200aa2a37fcaf6e5c9af898388172a0e66bb70d
Arian Stolwijk <stolwijk.arian@gmail.com>
2013-07-05 15:40:17 +0200

    Add function/partialLast.js
    Probably the most useful partial application function for prime

---
+++ test/function/partialLast.js
@@ -0,0 +1,32 @@
+"use strict"
+
+var expect = require("expect.js")
+var partialLast = require("../../function/partialLast")
+var filter = require("../../array/filter")
+var map = require("../../array/map")
+
+describe("function/partialLast", function(){
+
+    function even(x){
+        return x % 2 != 1
+    }
+
+    function multByThis(x){
+        return x * Number(this)
+    }
+
+    it("should partially apply the last arguments", function(){
+        var data = [0, 1, 2, 3, 4, 5]
+        var filterEven = partialLast(filter, even)
+        var evenNumbers = filterEven(data)
+        expect(evenNumbers).to.eql([0, 2, 4])
+    })
+
+    it("should partially apply the last two arguments", function(){
+        var data = [0, 1, 2, 3, 4, 5]
+        var multiplyWith10 = partialLast(map, multByThis, 10)
+        var multiplied = multiplyWith10(data)
+        expect(multiplied).to.eql([0, 10, 20, 30, 40, 50])
+    })
+
+})
